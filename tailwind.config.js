cf7cb9f8d9afee28d16306c99b656aedabe88480
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-11 14:37:48 +1200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      "primary": "#394C54",
+      "secondary": "#6BFF25"
+    },
+    fontFamily: {
+    },
+  },
+  plugins: [],
+}
