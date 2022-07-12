f6193342154e04852d619f20e5ac2b8b7abae549
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-12 12:28:22 +1200

    minor header component UI change

--- components/HeaderContainer.tsx
+++ components/HeaderContainer.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const HeaderContainer = () => {
   return (
-    <div className=''>
+    <div className='p-5 flex flex-col justify-between'>
     <div className='text-center sm:text-left sm:pl-[10px]'>
       <h1 className='text-6xl font-bold text-secondary  mb-2'>Apoteo</h1>
       <h3 className='text-3xl text-white'>The dynamic solution to pharmacy management</h3>
     </div>
 
-    <div className='flex w-full my-6'>
+    <div className='flex w-full my-6 sm:my-0'>
       <button className='bg-secondary p-4 rounded-md text-white w-full mx-[10px]'>Get Started</button>
       <button className='bg-secondary p-4 rounded-md text-white w-full mx-[10px]'>Learn more</button>
     </div>
