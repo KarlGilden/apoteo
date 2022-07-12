bbae03eb7d43729b6b532cb7fa0ba39e1ba51162
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-12 19:24:52 +1200

    b

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export default NextAuth({
         clientId: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
         issuer: process.env.AUTH0_ISSUER
-      })
-    // ...add more providers here
+      }),
   ],
   secret: process.env.NEXTAUTH_SECRET
+
 })
