982a05fd785f019c6a21359ee46465766d3ba27b
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-13 09:15:56 +1200

    add google auth import

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import Auth0Provider from "next-auth/providers/auth0";
+import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
