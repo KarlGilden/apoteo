5d5e4fc2e2cfc8ac905bb2e4b7254cd974933942
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-14 14:43:07 +1200

    fix another iteration bug

--- components/IssueItem.tsx
+++ components/IssueItem.tsx
@@ -9,9 +9,9 @@ const IssueItem = (props:any) => {
     <div className='w-full p-3 text-white border-primary-dark bg-primary-light border-b-[1px] border-primary-dark flex items-center justify-between'>
         <p>{props.value.title}</p>
         <div className='flex'>
-            {props.value.tags.split(',').map((tag:string) => {
+            {props.value.tags.split(',').map((tag:string, index:number) => {
                 return (
-                    <p className='m-1 border-[1px] border-white rounded-md text-white'>{tag}</p>
+                    <p key={index} className='m-1 border-[1px] border-white rounded-md text-white'>{tag}</p>
                 )
             })}
         </div>
