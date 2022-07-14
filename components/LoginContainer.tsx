477f06ea51a71c72663e38c325d9f3389b854e6d
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-14 14:47:26 +1200

    fix props error

--- components/LoginContainer.tsx
+++ components/LoginContainer.tsx
@@ -7,9 +7,9 @@ const LoginContainer = () => {
     <div className='w-full max-w-[400px] m-auto flex flex-col justify-between p-5  rounded-md' >
         <h2 className='text-5xl mb-3 text-white'>Login</h2>
 
-        <FormInput type={"text"} label={"Email"}/>
+        <FormInput setFormState={()=>{}} type={"text"} label={"Email"}/>
 
-        <FormInput type={"password"} label={"Password"}/>
+        <FormInput setFormState={()=>{}} type={"password"} label={"Password"}/>
 
         <button onClick={()=>{signIn()}} className='bg-secondary p-4 rounded-md text-white'>Login</button>
   </div>
