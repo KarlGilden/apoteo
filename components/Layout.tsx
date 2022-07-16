43b74dc5f71cd51f0792e579e3d29fc6ef977d9c
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-16 16:26:16 +1200

    update application name

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({children}:IProps) => {
   return (
     <div>
     <Head>
-      <title>Apoteo</title>
+      <title>WHOPs</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
       <Navbar/>
