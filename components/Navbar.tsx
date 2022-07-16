ac5580eccc02fbc85a224b70be49e26bee458410
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-16 16:26:05 +1200

    update nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className='w-full h-[72px] bg-primary-dark absolute flex items-center px-24'>
         <Link href="/"><a className='text-white'>Home</a></Link>
         <div className='p-2'></div>
-        <Link href="/"><a className='text-white'>New Log</a></Link>
+        <Link href="/new-log"><a className='text-white'>New Log</a></Link>
     </div>
   )
 }
