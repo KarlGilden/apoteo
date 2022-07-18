5ffde89fe9e03eef8c9de0acdfd889a6621f2fa2
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-18 13:55:35 +1200

    ws

--- components/DashboardContainer.tsx
+++ components/DashboardContainer.tsx
@@ -15,14 +15,14 @@ const DashboardContainer = () => {
         })
         .then(response => response.json())
         .then(data => {
-            setData(data)
+            setData(data[0].date)
             console.log(data)
         })  
     }
 
   return (
     <div className='bg-primary-light p-10'>
-        
+        {data}
     </div>
   )
 }
--- util/db/index.ts
+++ util/db/index.ts
@@ -2,9 +2,11 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
+    console.log(process.env.FIREBASE_CONFIG)
     admin.initializeApp({
       credential: admin.credential.cert(process.env.FIREBASE_CONFIG as admin.ServiceAccount)
     });
+
   } catch (error: any) {
     console.log('Firebase admin initialization error', error.stack);
   }
