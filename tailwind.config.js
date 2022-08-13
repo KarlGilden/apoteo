1670d3c965da0957130ca0e3208558231935cbc5
KarlGilden <karl.gildenhuys@gmail.com>
2022-08-13 12:06:19 +1200

    add more color definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       "primary-dark": "#2a2d34",
       "primary-light": "#414550",
       "secondary": "#22c55e",
+      "secondary-dark": "#1fb155",
+      "secondary-light": "#29da6a",
+      "secondary-dull": "#43644e",
       "white": "#fff",
       "black": "#000",
       "transparent": "transparent"
