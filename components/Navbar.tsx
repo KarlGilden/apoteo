91f53b319a1b5727a5455f3c7e49687fd193b433
KarlGilden <karl.gildenhuys@gmail.com>
2022-08-17 11:07:04 +1200

    add home nav link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,8 @@ const Navbar = () => {
   return (
     <>
     <nav className='w-full h-[72px] bg-white absolute flex items-center px-24'>
+        <Link href="/"><a className='text-dark-green'>Home</a></Link>
+        <div className='p-2'></div>
         <Link href="/dashboard"><a className='text-dark-green'>Dashboard</a></Link>
         <div className='p-2'></div>
         <Link href="/new-log"><a className='text-dark-green'>New Log</a></Link>
