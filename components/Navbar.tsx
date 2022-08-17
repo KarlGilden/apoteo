bb132bf0bfd92cf348fc13a33e23c2e28926c97f
KarlGilden <karl.gildenhuys@gmail.com>
2022-08-17 14:52:31 +1200

    fix nav spacing issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <nav className='w-full h-[72px] bg-white absolute flex items-center px-24'>
+    <nav className='w-full h-[72px] bg-white absolute flex justify-center items-center px-5 sm:justify-start sm:px-24'>
         <Link href="/"><a className='text-dark-green'>Home</a></Link>
         <div className='p-2'></div>
         <Link href="/dashboard"><a className='text-dark-green'>Dashboard</a></Link>
