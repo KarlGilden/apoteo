6edfb2cbb6fdc3a4746fa651017424ed9bb143d2
KarlGilden <karl.gildenhuys@gmail.com>
2022-09-04 20:59:59 +1200

    change interventions and error desc to text area

--- components/AddInterventionModal.tsx
+++ components/AddInterventionModal.tsx
@@ -62,7 +62,7 @@ const AddInterventionModal = ({show, handleClose, interventions, setIntervention
 
   const modal = show ? (
     <div className={`overflow-hidden fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50  flex justify-center items-center`}>
-      <div className='max-w-[600px] w-[90%] h-[90%] bg-white p-5 rounded-md flex flex-col justify-between'>
+      <div className='max-w-[600px] w-[90%] h-[90%] bg-white p-5 rounded-sm flex flex-col justify-between'>
 
         <div className='flex justify-between items-center'>
           <h1 className=' text-4xl'>New Intervention</h1>
@@ -71,15 +71,18 @@ const AddInterventionModal = ({show, handleClose, interventions, setIntervention
 
         <div>
           <FormInput setFormState={setIssueTitle} type={'text'} label={'Title'} />
-          <FormInput setFormState={setIssueDesc} type={'text'} label={'Description'} />
+          <div className='h-full'>
+            <label className='text-dark-green mb-1' htmlFor="">Description:</label>
+            <textarea className='w-full h-full max-h-[100px] resize-none border-off-white focus:outline-none focus:bg-off-white focus:border-[#111] text-dark-green rounded-md border-[2px] p-1 bg-transparent' onChange={(e)=>setIssueDesc(e.target.value)} ></textarea>
+          </div>
         </div>
 
         <div className='flex flex-wrap m-5'>
-          <p className={`p-2 m-2 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved serious harm') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved serious harm')}>Improved serious harm</p>
-          <p className={`p-2 m-2 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved harm') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved harm')}>Improved harm</p>
-          <p className={`p-2 m-2 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved efficacy') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved efficacy')}>Improved efficacy</p>
-          <p className={`p-2 m-2 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved compliance') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved compliance')}>Improved compliance</p>
-          <p className={`p-2 m-2 cursor-pointer border-2 rounded-md ${issueTags.includes('Incomplete / illegible') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Incomplete / illegible')}>Incomplete / illegible</p>
+          <p className={`text-sm px-1 py-[2px] m-1 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved serious harm') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved serious harm')}>Improved serious harm</p>
+          <p className={`text-sm px-1 py-[2px] m-1 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved harm') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved harm')}>Improved harm</p>
+          <p className={`text-sm px-1 py-[2px] m-1 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved efficacy') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved efficacy')}>Improved efficacy</p>
+          <p className={`text-sm px-1 py-[2px] m-1 cursor-pointer border-2 rounded-md ${issueTags.includes('Improved compliance') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Improved compliance')}>Improved compliance</p>
+          <p className={`text-sm px-1 py-[2px] m-1 cursor-pointer border-2 rounded-md ${issueTags.includes('Incomplete / illegible') ? 'bg-secondary text-white border-secondary' : 'bg-transparent border-off-white'}`} onClick={()=>toggleTags('Incomplete / illegible')}>Incomplete / illegible</p>
         </div>
 
         <button onClick={()=>{handleSubmit(); }} className='p-3 bg-secondary w-full'>Submit</button>
